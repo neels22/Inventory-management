fe761f7ead5ef03fc7c9e32b795ae0288ef49e04
neels22 <indraneelsarode@gmail.com>
2025-04-04 22:56:12 -0700

    initial file structure and basic code

---
+++ Backend/app/main.py
@@ -0,0 +1,7 @@
+
+
+# the entry point 
+
+from fastapi import Fastapi
+
+app = FastAPI(title="Inventory Management API")
---
+++ Backend/routers/inventory.py
@@ -0,0 +1,7 @@
+
+
+from fastapi import APIRouter, Depends, HTTPException
+
+router = APIRouter()
+
+# @router.post("/inventory")
