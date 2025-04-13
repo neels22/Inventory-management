75f05bc0e448739e172c55cdbe185b0750e50b03
neels22 <indraneelsarode@gmail.com>
2025-04-12 17:48:08 -0700

    base pydantic models

--- Backend/schema/product.py
+++ Backend/schema/product.py
@@ -1,4 +1,35 @@
 
 
 from pydantic import BaseModel
-from typing import Optional
+from datetime import datetime
+from typing import List, Optional, Dict
+
+
+class ProductBase(BaseModel):
+    id: int
+    code: Optional[str]
+    name: str
+    nickname: str
+    quantity: int
+    threshold: int
+    description: Optional[str]
+    price: float
+    category: Optional[str]
+    brand: Optional[str]
+    location: Optional[str]
+
+
+class SaleBase(BaseModel):
+    id: int
+    quantity: int
+    date: datetime
+    total_price: float
+    products: List[ProductBase]
+
+
+
+
+
+
+
+
