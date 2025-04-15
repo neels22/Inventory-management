8300d5c4480c135e9e9e01a542628987e64c59cd
neels22 <indraneelsarode@gmail.com>
2025-04-14 17:53:04 -0700

    figure out the bug

--- Backend/routers/inventory.py
+++ Backend/routers/inventory.py
@@ -4,6 +4,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from schema import ProductBase, SaleBase
 from typing import List, Optional, Dict
 
+from constants.products import products as product_data
+from constants.sales import sales as sales_data
+
 router = APIRouter()
 
 # @router.post("/inventory")
@@ -14,4 +17,5 @@ async def get_inventory():
     Get all products in the inventory.
     """
     # Logic to get all products in the inventory
-    return {"message": "Inventory retrieved successfully", "inventory": []}
+
+    return {"message": "Inventory retrieved successfully", "inventory": product_data}
